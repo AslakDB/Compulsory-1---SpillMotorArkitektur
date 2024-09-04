4c35a80c88dcba07055876c7c4c93bc64f99aa67
AslakDB <Askelakken@gmail.com>
2024-09-04 15:07:15 +0200

    Added everytinh from hollow engine for the project to run
    Next step is to ensure shaders are working as inteded, then add walls and floor, hopefully with one class.

--- Compulsory1/Compulsory1/Compulsory1.cpp
+++ Compulsory1/Compulsory1/Compulsory1.cpp
@@ -1,9 +1,35 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
+
+#include "Setup.h"
+#include "render.h"
 #include <iostream>
+
+SetUp set_up;
+Render render;
+
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f;
+
 
 int main(int argc, char* argv[])
 {
-    
+    GLFWwindow* window;
+
+    set_up.setup("Window", window, SCR_WIDTH,SCR_HEIGHT);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    unsigned int shadersProgram = shaders.GetProgram();
+    glUseProgram(shadersProgram);
+
+
+
+
+    render.render(window , shadersProgram, deltaTime, lastFrame);
+
+    glfwTerminate();
 }
---
+++ Compulsory1/Compulsory1/render.h
@@ -0,0 +1,204 @@
+﻿//
+// Created by askel on 4/2/2024.
+//
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Camera.h"
+#include "Model.h"
+
+
+#ifndef RENDER_H
+#define RENDER_H
+Camera camera;
+
+bool firstMouse = true;
+
+float lastX = 960, lastY = 540;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+void ProsessInput(GLFWwindow *window, float deltaTime);
+
+
+struct Render {
+bool inside;
+    bool isMovingForward;
+
+   // model NpcGraph;
+
+    // std::vector<model*> models;
+    // model ThePlane;
+    // model PlayerBox;
+    // model NpcBox;
+
+
+
+    void render(GLFWwindow* window, unsigned int shaderProgram, float deltaTime, float lastFrame) {
+
+        // models.emplace_back(&ThePlane);
+        // models.emplace_back(&PlayerBox);
+        // models.emplace_back(&NpcGraph);
+        // models.emplace_back(&NpcBox);
+
+        glm::mat4 trans = glm::mat4(1.0f);
+        glm::mat4 projection;
+
+        // CreateMeshPlane(ThePlane, 20, 20);
+        //
+        //
+        //
+        //
+        // CreateMeshBox(PlayerBox);
+        // CreateMeshBox(NpcBox);
+
+      
+
+        
+
+        // bool isMovingforward = true;
+        //
+        // float NpcXPos = 3.0f;
+        // float NpcYPos = 0.0f;
+        // float NpcZPos = f(NpcXPos);
+
+        while (!glfwWindowShouldClose(window))
+            {
+
+            float currentFrame = glfwGetTime();
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+            ProsessInput(window, deltaTime);
+
+            projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+            camera.tick(shaderProgram);
+
+            /*for (auto element: ThePlane.indices) {
+                calculateBarycentric(ThePlane.vertices[element.A], ThePlane.vertices[element.B]
+                    ,ThePlane.vertices[element.C], PlayerBox.PlayerPos );
+            }
+            PlayerBox.modelMatrix = glm::translate(glm::mat4(1.f), PlayerBox.PlayerPos);*/
+
+
+
+
+            /*MoveNPC(NpcBox, glm::vec3(NpcXPos, NpcYPos, NpcZPos));
+            if (NpcXPos > 8.25f)
+            {
+                isMovingForward = false;
+            }
+            if (NpcXPos < 2.0f)
+            {
+                isMovingForward = true;
+            }
+
+            if (isMovingForward)
+            {
+                NpcXPos += 1 * deltaTime;
+                NpcZPos = f(NpcXPos);
+            }
+            else
+            {
+                NpcXPos -= 1 * deltaTime;
+                NpcZPos = f(NpcXPos);
+            }
+
+
+            for (auto element: ThePlane.indices) {
+                calculateBarycentric(ThePlane.vertices[element.A], ThePlane.vertices[element.B]
+                    ,ThePlane.vertices[element.C], NpcBox.PlayerPos );
+            }
+
+            NpcBox.modelMatrix = glm::translate(glm::mat4(1.f), NpcBox.PlayerPos);*/
+            glClearColor(0.5f, 0.99f, 0.5f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            int viewLoc = glGetUniformLocation(shaderProgram, "viewPos");
+            glUniform3fv(viewLoc, 1, glm::value_ptr(camera.cameraPos));
+
+            int LightLoc = glGetUniformLocation(shaderProgram, "lightPos");
+            glUniform3fv(LightLoc, 1, glm::value_ptr(glm::vec3(5,20,0)));
+
+            glLineWidth(3);
+
+// This has to be undone, but after we have made some models to draw
+            /*for (model* element: models) {
+                element->DrawMesh(shaderProgram);
+            }*/
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+            }
+    }
+};
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    camera.yaw   += xoffset;
+    camera.pitch += yoffset;
+
+    if(camera.pitch > 89.0f)
+        camera.pitch = 89.0f;
+    if(camera.pitch < -89.0f)
+        camera.pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+    direction.y = sin(glm::radians(camera.pitch));
+    direction.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+    camera.cameraFront = glm::normalize(direction);
+}
+
+
+void ProsessInput(GLFWwindow *window, float deltaTime) {
+
+    glm::vec3 cameraFrontXZ = glm::normalize(glm::vec3(camera.cameraFront.x, 0.0f, camera.cameraFront.z));
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = 2.5f * deltaTime ;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera.cameraPos += cameraSpeed * cameraFrontXZ;
+        /*Player.PlayerPos +=  cameraSpeed * cameraFrontXZ;*/}
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+       { camera.cameraPos -= cameraSpeed * cameraFrontXZ;
+        /*Player.PlayerPos -=  cameraSpeed * cameraFrontXZ;*/}
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+        camera.cameraPos -= glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
+    /*Player.PlayerPos -=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+        camera.cameraPos += glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
+    /*Player.PlayerPos +=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.cameraPos += cameraSpeed * camera.cameraUp; // Move camera up
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.cameraPos -= cameraSpeed * camera.cameraUp;
+
+    /*if(glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        Player.PlayerPos +=  glm::vec3(1.f* deltaTime,0,0);
+    if(glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+        Player.PlayerPos +=  glm::vec3(0,0,-1.f* deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+
+    if(glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+        Player.PlayerPos +=  glm::vec3(0,0,1.f* deltaTime);*/
+
+}
+#endif //RENDER_H
